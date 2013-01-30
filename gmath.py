a86a6baab05aa3b4a7b37d80f7b77e83a9c95249
Gregory Gundersen <ggundersen@gmail.com>
2013-01-30 00:00:23 -0500

    in progress

--- gmath.py
+++ gmath.py
@@ -54,7 +54,7 @@ def gen_primes_from_p(p):
             yield n
 
 
-def sieve_of_atkin(limit):
+def sieve_of_atkin(ldown,imit):
 
     # initialize
     primes = [2,3,5]
@@ -85,6 +85,38 @@ def sieve_of_atkin(limit):
         if sieve[i]:
             primes.append(i)
     return primes
+
+def gen_sieve_of_eratosthenes():
+    # Code by David Eppstein, UC Irvine, 28 Feb 2002
+
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+ 
+    D = {}  
+
+    # The running integer that's checked for primeness
+    q = 2  
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+
+            yield q        
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next 
+            # multiples of its witnesses to prepare for larger
+            # numbers
+
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
 
 def get_prime_factors(n):
     primeFactors = []
--- test.py
+++ test.py
@@ -1,27 +1,22 @@
-'''
-DESCRIPTION:
-Project Euler, problem 41
-Gregory Gundersen, ../../2013
-
-PROBLEM:
-We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once.
-For example, 2143 is a 4-digit pandigital and is also prime.
-
-What is the largest n-digit pandigital prime that exists?
-
-SOLUTION:
-7652413
-'''
-
-import time
 import gmath as g
 
-s = time.time()
+'''
+def is_circular_prime(p):
+    for i in range(len(str(p))):
+        if g.isPrime(p) == False:
+            return False
+        else:
+            sp = str(p)
+            for char in sp:
+'''
 
-results = []
-for n in range(7654321, 7000000, -2):
-    if g.is_pandigital(n) and g.isPrime(n):
-        results.append(n)
+s = '12345'
+sl = list(s)
 
-print max(results)
-print 'Time: ' + str(time.time() - s)
+s_first = sl[0]
+for i in range(0,len(s)-1):
+    sl[i] = sl[i+1]
+    print sl
+sl[len(s)-1] = s_first
+
+print sl
